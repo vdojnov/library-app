353f6eab7108f1dd33dac17a9c4a719363f4fca9
Viktor Dojnov <dojnov.viktor@gmail.com>
2020-12-26 03:15:42 -0800

    finish book div arrangement

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ function Book(title, author, pages, read, category) {
     this.info = function() {    
         return title + " by " + author+ ", " + pages +" pages, " + read?"finished reading.":"not yet read."; 
     }
+    this.getThumbnail = function() {
+        const Http = new XMLHttpRequest();
+        const url='https://www.googleapis.com/books/v1/volumes?q=' + "Think and Grow Rich";
+        Http.open("GET", url);
+        Http.send();
+
+        Http.onreadystatechange = (e) => {
+            // console.log(Http.responseText)
+            let objJson = JSON.parse(Http.responseText);
+            // console.log(objJson.items[0].volumeInfo.imageLinks.thumbnail)
+            this.thumbnail=objJson.items[0].volumeInfo.imageLinks.thumbnail
+        
+        }
+    }
 }
 
 function addBookToLibrary(bookObj) {
